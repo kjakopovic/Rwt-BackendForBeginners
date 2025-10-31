9b8814da5da6c5e31bb22b00b7bc7fb6239fa363
kjakopovic <karlojakopov@gmail.com>
2025-10-31 08:32:19 +0100

    feat(Models): added User model definition

---
+++ BackendForBeginners-Solution/Models/User.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BackendForBeginners_Solution.Models;
+
+public class User
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public Guid Id { get; set; }
+
+    [MaxLength(50)]
+    public required string Username { get; set; }
+
+    [MaxLength(100)]
+    [EmailAddress]
+    public required string Email { get; set; }
+
+    public required string Password { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+    public DateTime? DeletedAt { get; set; }
+}
