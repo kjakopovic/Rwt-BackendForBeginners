b23aee365a559beebc363f4542915139a55e3f78
kjakopovic <karlojakopov@gmail.com>
2025-10-31 09:48:36 +0100

    fix(Migration-And-Database): setup finished for database + migrated to new .NET 10 application

---
+++ BackendForBeginners-Net10-Solution/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+using BackendForBeginners_Net10_Solution.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackendForBeginners_Net10_Solution.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; } = null!;
+}
+
---
+++ BackendForBeginners-Net10-Solution/Program.cs
@@ -0,0 +1,13 @@
+using BackendForBeginners_Net10_Solution.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
