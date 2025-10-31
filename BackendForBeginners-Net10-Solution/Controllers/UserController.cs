b1237ca4aa090939f0f921ff59d9f1c544c9c844
kjakopovic <karlojakopov@gmail.com>
2025-10-31 21:34:41 +0100

    chore(Controller-Service): implemented service in controller

--- BackendForBeginners-Net10-Solution/Controllers/UserController.cs
+++ BackendForBeginners-Net10-Solution/Controllers/UserController.cs
@@ -1,58 +1,38 @@
 using BackendForBeginners_Net10_Solution.Dtos;
+using BackendForBeginners_Net10_Solution.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BackendForBeginners_Net10_Solution.Controllers;
 
 [Route("api/users")]
 [ApiController]
-public class UserController(AppDbContext context) : ControllerBase
+public class UserController(IUserService userService) : ControllerBase
 {
-    private readonly AppDbContext _context = context;
+    private readonly IUserService _userService = userService;
 
     [HttpGet]
-    public IActionResult GetUsers()
+    public async Task<IActionResult> GetUsers()
     {
-        var users = _context.Users.ToList();
+        var users = await _userService.GetUsers(null);
         return Ok(users);
     }
 
     [HttpGet("{id}")]
-    public IActionResult GetUser(Guid id)
+    public async Task<IActionResult> GetUser(Guid id)
     {
-        var user = _context.Users.Find(id);
-        if (user == null)
-        {
-            return NotFound();
-        }
-
+        var user = await _userService.GetUsers(id);
         return Ok(user);
     }
 
     [HttpPost]
-    public IActionResult CreateUser([FromBody] UserDto user)
+    public async Task<IActionResult> CreateUser([FromBody] UserDto user)
     {
         if (!ModelState.IsValid)
         {
             return BadRequest(ModelState);
         }
 
-        var existingUser = _context.Users.FirstOrDefault(u => u.Email == user.Email);
-        if (existingUser != null)
-        {
-            return Conflict(new { message = "A user with this email already exists." });
-        }
-
-        var hashedPassword = BCrypt.Net.BCrypt.HashPassword(user.Password);
-
-        var newUser = new Models.User
-        {
-            Username = user.Username,
-            Email = user.Email,
-            Password = hashedPassword
-        };
-
-        _context.Users.Add(newUser);
-        _context.SaveChanges();
+        var newUser = await _userService.CreateUser(user);
 
         return Ok(new
         {
@@ -65,21 +45,9 @@ public class UserController(AppDbContext context) : ControllerBase
     }
 
     [HttpPut("{id}")]
-    public IActionResult UpdateUser(Guid id, [FromBody] UserDto updatedUser)
+    public async Task<IActionResult> UpdateUser(Guid id, [FromBody] UserDto updatedUser)
     {
-        var user = _context.Users.Find(id);
-        if (user == null)
-        {
-            return NotFound();
-        }
-
-        user.Username = updatedUser.Username;
-        user.Email = updatedUser.Email;
-        user.Password = BCrypt.Net.BCrypt.HashPassword(updatedUser.Password);
-        user.UpdatedAt = DateTime.UtcNow;
-
-        _context.Users.Update(user);
-        _context.SaveChanges();
+        var user = await _userService.UpdateUser(updatedUser, id);
 
         return Ok(new
         {
@@ -92,17 +60,12 @@ public class UserController(AppDbContext context) : ControllerBase
     }
 
     [HttpDelete("{id}")]
-    public IActionResult DeleteUser(Guid id)
+    public async Task<IActionResult> DeleteUser(Guid id)
     {
-        var user = _context.Users.Find(id);
-        if (user == null)
+        await _userService.DeleteUser(id);
+        return Ok(new
         {
-            return NotFound();
-        }
-
-        _context.Users.Remove(user);
-        _context.SaveChanges();
-
-        return Ok();
+            Message = "User deleted successfully"
+        });
     }
 }
